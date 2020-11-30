bf8c6e442d7c5ea66f335855d4e17efc0da9fb06
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-11-30 21:18:26 +0900

    Radios

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  bool _value1 = false;
-  bool _value2 = false;
+  int _value1 = 0;
+  int _value2 = 0;
 
-  void _value1Changed(bool value) {
+  void _setValue1(int value) {
     setState(() => _value1 = value);
   }
 
-  void _value2Changed(bool value) {
+  void _setValue2(int value) {
     setState(() => _value2 = value);
+  }
+
+  Widget makeRadios() {
+    List<Widget> list = new List<Widget>();
+
+    for (int i = 0; i < 3; i++) {
+      list.add(new Radio(value: i, groupValue: _value1, onChanged: _setValue1));
+    }
+    Column column = new Column(
+      children: list,
+    );
+    return column;
+  }
+
+  Widget makeRadioTiles() {
+    List<Widget> list = new List<Widget>();
+
+    for (int i = 0; i < 3; i++) {
+      list.add(new RadioListTile(
+        value: i,
+        groupValue: _value2,
+        onChanged: _setValue2,
+        activeColor: Colors.green,
+        controlAffinity: ListTileControlAffinity.trailing,
+        title: new Text('Item $i'),
+        subtitle: new Text('sub title'),
+      ));
+    }
+    Column column = new Column(
+      children: list,
+    );
+    return column;
   }
 
   @override
@@ -34,16 +66,8 @@ class _State extends State<MyApp> {
           child: new Center(
             child: new Column(
               children: <Widget>[
-                new Checkbox(value: _value1, onChanged: _value1Changed),
-                new CheckboxListTile(
-                  value: _value2,
-                  onChanged: _value2Changed,
-                  title: new Text('Hello world'),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  subtitle: new Text('Subtitle'),
-                  secondary: Icon(Icons.archive),
-                  activeColor: Colors.red,
-                )
+                makeRadios(),
+                makeRadioTiles(),
               ],
             ),
           ),
